c4d4379769a29ab7128b6162526798733f02f0ea
Makan DIANKA <makan.dianka@hotmail.com>
2023-06-13 04:58:18 +0200

    token authentication

--- restframework/settings.py
+++ restframework/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     'blog',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
