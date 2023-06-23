96952bf744a107d0f54148a83cdc5464b19a7d33
Makan DIANKA <makan.dianka@hotmail.com>
2023-06-23 03:51:43 +0200

    required token authentication for all endpoint

--- blog/views/blog_delete.py
+++ blog/views/blog_delete.py
@@ -1,10 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .. serializers.blog import BlogSerialiser
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from .. models.blog import Blog
 from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def blog_delete(request, id):
     post = get_object_or_404(Blog, pk=id)
     post.delete()
